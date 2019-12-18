25bc73c9157067d9b58f5dbd5d14f31f577f9163
jkoopman-42 <jkoopman@student.codam.nl>
2019-12-18 16:47:44 +0100

    Returnvalues fix

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: jkoopman <jkoopman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/12/07 16:20:48 by jkoopman       #+#    #+#                */
-/*   Updated: 2019/12/16 12:17:32 by jkoopman      ########   odam.nl         */
+/*   Updated: 2019/12/18 12:11:58 by jkoopman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,15 @@ int		ft_putnbr_base(unsigned long long num, size_t base, int sign, int uc)
 {
 	unsigned long long	nbr;
 	char				c;
+	int					len;
 
+	len = 0;
 	if (base < 2 || base > 36)
 		return (-1);
 	if (num >> 63 & 1 && sign)
 	{
 		if (base == 10)
-			write(1, "-", 1);
+			len += write(1, "-", 1);
 		else
 			num &= ~1;
 		nbr = (num * -1);
@@ -31,28 +33,32 @@ int		ft_putnbr_base(unsigned long long num, size_t base, int sign, int uc)
 	else
 		nbr = num;
 	if (nbr >= base)
-		ft_putnbr_base(nbr / base, base, sign, uc);
+		len += ft_putnbr_base(nbr / base, base, sign, uc);
 	c = CHARSET_BASE[nbr % base];
 	if (uc == 0)
 		c |= 32;
-	return (write(1, &c, 1));
+	len += write(1, &c, 1);
+	return (len);
 }
 
 int		ft_putint_base(long num, size_t base, int uc)
 {
 	char	c;
+	int		len;
 
+	len = 0;
 	if (base < 2 || base > 36)
 		return (-1);
 	if (num < 0)
 	{
-		write(1, "-", 1);
+		len += write(1, "-", 1);
 		num *= -1;
 	}
 	if (num >= (long)base)
-		ft_putint_base(num / base, base, uc);
+		len += ft_putint_base(num / base, base, uc);
 	c = CHARSET_BASE[num % base];
 	if (uc == 0)
 		c |= 32;
-	return (write(1, &c, 1));
+	len += write(1, &c, 1);
+	return (len);
 }
