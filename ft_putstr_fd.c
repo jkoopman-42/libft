bbaad0beef9d7eff98baf26b2bbf70eb8cc2a70e
jkoopman-42 <jkoopman@student.codam.nl>
2020-01-14 19:16:46 +0100

    putstring is faster now

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,22 +6,15 @@
 /*   By: jkoopman <jkoopman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/01 11:58:01 by jkoopman       #+#    #+#                */
-/*   Updated: 2019/11/09 12:33:12 by jkoopman      ########   odam.nl         */
+/*   Updated: 2020/01/14 17:08:10 by jkoopman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	size_t	i;
-
 	if (s == NULL)
 		return ;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
+	write(fd, s, ft_strlen(s));
 }
